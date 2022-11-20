a742551c122381a28f99e776866ae8ca834bafc5
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-11-20 19:15:34 +0300

    change landing page contents and delete components

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,19 +1,9 @@
 import { FC } from 'react';
-import Navbar from '../components/landing-page/Navbar';
 
 const LandingPage: FC = () => {
   return (
     <>
-      <Navbar pressed="Home" />
-
-      <main className="ml-80 mt-6">
-        <div className="flex justify-center mt-4">
-          <img src="/banner.png" height={512} width={1024} />
-        </div>
-        <h1 className="text-2xl flex justify-center">
-          The chat app that actually doesn't suck
-        </h1>
-      </main>
+      <h1>under construction</h1>
     </>
   );
 };
